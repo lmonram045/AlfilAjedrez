5d903fcb42d52cd2ea28f59b0337de2454fab3a8
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-03-05 13:19:18 +0100

    Punto 6: Creados get y set para Posicion con la visibilidad adecuada y creadas las excepciones

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -4,4 +4,33 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			/*Hubiera creado una constante para el texto del error, pero dejo un literal
+			  para ajustarme a la definicion de la clase */
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		} else {
+				this.fila = fila;
+		}
+		
+	}
+	public int getFila() {
+		return fila;
+	}
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			/*Hubiera creado una constante para el texto del error, pero dejo un literal
+			  para ajustarme a la definicion de la clase */
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		} else {
+			this.columna = columna;	
+		}
+		
+	}
+	public char getColumna() {
+		return columna;
+	}
+	
+	
+	
 }
