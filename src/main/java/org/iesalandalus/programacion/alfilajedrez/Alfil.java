051be2379317c3c98595d991831f5234c88887a5
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-03-11 17:01:43 +0100

    Punto 12: Creados metodos get y set.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -4,4 +4,23 @@ public class Alfil {
 	private Color color;
 	private Posicion posicion;
 	
+	public Color getColor() {
+		return color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}
+		this.color = color;
+	}
+	private void setPosicion(Posicion posicion) {
+		this.posicion = new Posicion(posicion);
+
+		
+	}
+	
+
 }
