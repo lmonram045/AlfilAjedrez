1df5a2e758c001b30ead8d442f2036f41e6975c3
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-03-12 11:53:50 +0100

    Punto 16.
    Se crea el método mover que acepta como parámetros direccion y pasos
    lanzando las excepciones adecuadas

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Alfil {
 	private Color color;
 	private Posicion posicion;
@@ -38,6 +40,70 @@ public class Alfil {
 			posicion = new Posicion(8, columna);
 		}
 		this.color = color;
+	}
+	
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+		if (direccion == null) {
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+		if (pasos < 1) {
+			throw new IllegalArgumentException("ERROR: El número de pasos debe ser positivo.");
+		}
+		
+		/*if (posicion.getColumna < 'a' || posicion.getColumna > 'h' || posicion.getFila < 1 || posicion.getFila > 8) {
+			throw new Operation
+		}*/
+		Posicion posicionDestino;
+		int filaDestino;
+		char columnaDestino;
+		try {
+		switch (direccion) {
+         case ARRIBA_DERECHA:  
+        	filaDestino = this.posicion.getFila() + pasos;
+			columnaDestino = this.posicion.getColumna();
+			
+			for (int i = 1; i <= pasos; i++) {
+				columnaDestino++;
+			}
+			posicionDestino = new Posicion(filaDestino, columnaDestino);
+            break;
+         case ARRIBA_IZQUIERDA:  
+        	filaDestino = this.posicion.getFila() + pasos;
+ 			columnaDestino = this.posicion.getColumna();
+ 			
+ 			for (int i = 1; i <= pasos; i++) {
+ 				columnaDestino--;
+ 			}
+ 			posicionDestino = new Posicion(filaDestino, columnaDestino);
+            break;
+         case ABAJO_DERECHA:  
+        	filaDestino = this.posicion.getFila() - pasos;
+ 			columnaDestino = this.posicion.getColumna();
+ 			
+ 			for (int i = 1; i <= pasos; i++) {
+ 				columnaDestino++;
+ 			}
+ 			posicionDestino = new Posicion(filaDestino, columnaDestino);
+            break;
+         case ABAJO_IZQUIERDA:  
+        	filaDestino = this.posicion.getFila() - pasos;
+ 			columnaDestino = this.posicion.getColumna();
+ 			
+ 			for (int i = 1; i <= pasos; i++) {
+ 				columnaDestino--;
+ 			}
+ 			posicionDestino = new Posicion(filaDestino, columnaDestino);
+            break;
+            
+         default: 
+        	 throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+		}
+		} catch (IllegalArgumentException e) {
+			throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+		}
+		
+		this.setPosicion(posicionDestino);		
+		
 	}
 	
 	public Color getColor() {
