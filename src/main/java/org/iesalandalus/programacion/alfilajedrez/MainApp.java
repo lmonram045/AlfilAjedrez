8e68e5416e1e825633f8dbfd4ff3f56fb528cbf7
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-03-12 13:15:26 +0100

    Punto 19.i
    Creada la clase MainApp, método main (vacío por ahora), y el método
    voistrarAlfil, el cual me muestra la posición y el color de un alfil
    creado

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -0,0 +1,27 @@
+package org.iesalandalus.programacion.alfilajedrez;
+
+
+
+public class MainApp {
+	
+	private static Alfil alfil = null;
+	
+	private static void mostrarAlfil() {
+		if (alfil != null) {
+			System.out.println("Alfil: " + alfil);
+		} else {
+			System.out.println("Aún no se ha creado ningún alfil.");
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+	
+		
+		
+	
+	}
+
+	
+
+}
